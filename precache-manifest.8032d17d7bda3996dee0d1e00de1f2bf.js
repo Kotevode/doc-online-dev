e1437c1451bfe10213ab66e7aa5559e74fb68ef5
Mark <kotevode@icloud.com>
2019-02-11 22:48:21 +0500

    Updates

---
+++ precache-manifest.8032d17d7bda3996dee0d1e00de1f2bf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "40ccec13752ce1f19ec1b0658906aace",
+    "url": "./static/media/Icon14Statoscope.40ccec13.svg"
+  },
+  {
+    "revision": "0299f315280356bf5eba",
+    "url": "./static/css/main.c76161f7.chunk.css"
+  },
+  {
+    "revision": "e5a45960f96b317e7215490043453288",
+    "url": "./static/media/OrderSuccessCheckmark.e5a45960.svg"
+  },
+  {
+    "revision": "ebc5e7a860b8d40a83ee",
+    "url": "./static/js/1.ebc5e7a8.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ee2f3db301c63da487e5b0dd28157691",
+    "url": "./static/media/AvatarPlaceholder.ee2f3db3.svg"
+  },
+  {
+    "revision": "f986346a666188df98e8b604647f2780",
+    "url": "./static/media/HomeBackground.f986346a.svg"
+  },
+  {
+    "revision": "0299f315280356bf5eba",
+    "url": "./static/js/main.0299f315.chunk.js"
+  },
+  {
+    "revision": "ceb67ee6f45d9665faf8781a3730f6e1",
+    "url": "./static/media/Icon14Pin.ceb67ee6.svg"
+  },
+  {
+    "revision": "e8eeca859aeda5b1ab529f4ddebe6670",
+    "url": "./static/media/Icon14Calendar.e8eeca85.svg"
+  },
+  {
+    "revision": "f1d95a5efb8e5154a3e37d41a2131943",
+    "url": "./static/media/Icon14Subway.f1d95a5e.svg"
+  },
+  {
+    "revision": "7c5f63e403c4a531ce2d3ac89cc16631",
+    "url": "./static/media/Icon12Star.7c5f63e4.svg"
+  },
+  {
+    "revision": "763bf14dce210e6b483202b7e34747b6",
+    "url": "./static/media/Icon13Pedestrian.763bf14d.svg"
+  },
+  {
+    "revision": "ebc5e7a860b8d40a83ee",
+    "url": "./static/css/1.f578053f.chunk.css"
+  },
+  {
+    "revision": "bf6b3b206fde181ffa8d50977ae875c0",
+    "url": "./index.html"
+  }
+];
